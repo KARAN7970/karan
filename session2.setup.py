1ea04761e1291218407e7346edca7930a3db8369
KARAN7970 <119036999+KARAN7970@users.noreply.github.com>
2023-07-03 20:25:51 +0530

    Add files via upload

---
+++ session2.setup.py
@@ -0,0 +1,12 @@
+student_marks = [20,20,66,40,50,60,70,80,90]
+student_marks2 = [22,33]
+#print(new_marks)
+
+#ast_marks = student_marks.pop(-1)
+#print(last_marks)
+
+#student_marks.remove(10)
+#print(student_mark
+
+student_marks.remove(20)
+print(student_marks)
