407f560bc67005771674f9952733acce336bb5c2
KARAN7970 <119036999+KARAN7970@users.noreply.github.com>
2023-07-19 21:41:04 +0530

    Add files via upload

---
+++ ass 1 .py
@@ -0,0 +1,26 @@
+
+
+
+# by using while loop
+x,y = 0,1
+
+while y<50:
+    print(y)
+    x,y = y,x+y
+
+
+    #by using the for loop 
+
+n = 9
+n1 = 0
+
+n2 = 1
+
+for i in range (1,n+1):
+
+    n3 = n1 +n2
+
+      
+    n1 = n2
+    n2 = n3
+    print(n1)
