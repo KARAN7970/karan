d2bd08ad9343812f6d04862fa790d52a8c58fa91
KARAN7970 <119036999+KARAN7970@users.noreply.github.com>
2023-08-02 19:20:45 +0530

    Add files via upload

---
+++ assigmant3(1).py
@@ -0,0 +1,12 @@
+def sum_numbers(numbers_list):
+    total_sum = sum(numbers_list)
+    return total_sum
+
+# Sample List
+sample_list = [8, 2, 3, 0, 7]
+
+# Calculating the sum of numbers in the list
+result = sum_numbers(sample_list)
+
+# Printing the output
+print(result)
