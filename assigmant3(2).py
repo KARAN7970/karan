f24aac7408f4b70855e8f9e797560f050b3dede7
KARAN7970 <119036999+KARAN7970@users.noreply.github.com>
2023-08-02 19:23:00 +0530

    Add files via upload

---
+++ assigmant3(2).py
@@ -0,0 +1,12 @@
+def reverse_string(input_string):
+    reversed_string = input_string[::-1]
+    return reversed_string
+
+# Sample String
+sample_string = "1234abcd"
+
+# Reversing the string
+result = reverse_string(sample_string)
+
+# Printing the output
+print(result)
